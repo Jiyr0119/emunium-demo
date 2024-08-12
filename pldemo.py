1c89e5293546860cb897d06deb1e536a860efa0f
高俊杰 <gaojunjie@sunline.cn>
2024-08-12 15:03:55 +0800

    add try catch

--- pldemo.py
+++ pldemo.py
@@ -3,9 +3,9 @@ from playwright.async_api import async_playwright
 from emunium import EmuniumPlaywright
 from pyautogui import ImageNotFoundException
 
+
 async def main():
     async with async_playwright() as p:
-        # 启动浏览器
         browser = await p.chromium.launch(
             executable_path='/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
             headless=False
@@ -13,26 +13,31 @@ async def main():
         page = await browser.new_page()
         emunium = EmuniumPlaywright(page)
 
-        # 访问 DuckDuckGo 网站
-        await page.goto('https://duckduckgo.com/')
+        await page.goto('https://accounts.google.com/accountchooser/identifier?continue=https%3A%2F%2Faccounts.google.com%2FManageAccount%3Fnc%3D1&ddm=0&flowName=GlifWebSignIn&hl=zh-CN&ifkv=AdF4I76hopAOI_8OXuBuWoRWbVpPVtLJon2KZQww4prN9ReOqxIgNKw0WKDLJucL2O-xQxSehLc06w&flowEntry=AccountChooser')
 
         try:
-            # 尝试使用 Emunium 进行操作
-            element = await page.wait_for_selector('[data-state="suggesting"]')
-            await emunium.type_at(element, 'Automating searches')
+            # 使用 Emunium 进行操作
+            element = await page.wait_for_selector('#identifierId')
+            print("Emunium: Found element for typing.")
+            await emunium.type_at(element, 'gaojonathan7@gmail.com')
+            print("Emunium: Typed text into the element.")
 
-            submit = await page.wait_for_selector('[aria-label="Search"]')
+            submit = await page.wait_for_selector('#identifierNext')
+            print("Emunium: Found search button.")
             await emunium.click_at(submit)
+            print("Emunium: Clicked search button.")
         except (ImageNotFoundException, Exception) as e:
             print(f"Emunium 操作失败，错误信息: {e}. 使用 Playwright 进行操作。")
 
-            # 如果 Emunium 操作失败，使用 Playwright API 进行操作
+            # 使用 Playwright API 进行操作
             try:
-                search_input = await page.wait_for_selector('[name="q"]')
-                await search_input.type('Automating searches')
+                search_input = await page.wait_for_selector('#identifierId')
+                await search_input.type('gaojonathan7@gmail.com')
+                print("Playwright: Typed text into the search input.")
 
-                search_button = await page.wait_for_selector('[aria-label="Search"]')
+                search_button = await page.wait_for_selector('#identifierNext')
                 await search_button.click()
+                print("Playwright: Clicked search button.")
             except Exception as e:
                 print(f"Playwright 操作也失败了，错误信息: {e}")
 
